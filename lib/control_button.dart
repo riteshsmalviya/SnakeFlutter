04735e7aed811fbecf7426654ee600e7f4deb990
riteshsmalviya <115831260+riteshsmalviya@users.noreply.github.com>
2024-03-12 13:43:21 +0530

    hello

---
+++ lib/control_button.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'direction.dart';
+
+class ControlPanel extends StatelessWidget {
+  final void Function(Direction direction)? onSwipe;
+
+  const ControlPanel({Key? key, this.onSwipe}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: 0.0,
+      right: 0.0,
+      bottom: 50.0,
+      child: GestureDetector(
+        onVerticalDragEnd: (details) {
+          if (details.primaryVelocity! > 0) {
+            onSwipe?.call(Direction.down);
+          } else if (details.primaryVelocity! < 0) {
+            onSwipe?.call(Direction.up);
+          }
+        },
+        onHorizontalDragEnd: (details) {
+          if (details.primaryVelocity! > 0) {
+            onSwipe?.call(Direction.right);
+          } else if (details.primaryVelocity! < 0) {
+            onSwipe?.call(Direction.left);
+          }
+        },
+        child: Container(
+          height: 150.0,
+          color: Colors.transparent, // Make the swipe gesture area transparent
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: Container(),
+              ),
+              Expanded(
+                child: Container(), // This is the center column, no need for specific controls here
+              ),
+              Expanded(
+                child: Container(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
